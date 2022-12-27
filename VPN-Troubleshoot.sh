381652bb2c39c3ab50198101243a060e34073eee
dpgg <d.g.23345@gmail.com>
2022-12-27 23:37:15 +0200

    Update VPN-Troubleshoot.sh
    - Added VPN file input
    - Added warning
    - More intuitive errors
    - Nmap scan

--- VPN-Troubleshoot.sh
+++ VPN-Troubleshoot.sh
@@ -1,44 +1,69 @@
 #!/bin/bash
-VERSION=0.1
-Author=dpgg
+#Author: dpgg
 
-
-### Colours ###
+# Define colors for output text
 RED="\033[01;31m"
 GREEN="\033[0;32m"
-ORG="\033[00m" ## Original color 
+ORG="\033[00m" # Original color
+YELLOW="\033[0;33m"
+#!/bin/bash
+
+# Author: dpgg
+
+# Define colors for output text
+RED="\033[01;31m"
+GREEN="\033[0;32m"
+ORG="\033[00m" # Original color
 YELLOW="\033[0;33m"
 
-## Get user and UID ##
-_user="$(id -u -n)"
-_uid="$(id -u)"
-_group="$(groups | awk '{print $1}')"
+# Get current user and UID
+user=$(id -u -n)
+uid=$(id -u)
+group=$(groups | awk '{print $1}')
 
-## User enum
+# Print user information
 echo -e "${GREEN} ====== USER RECON ====== ${ORG}"
-echo -e "User name:${RED} $_user ${ORG}"
-echo -e "User name ID (UID):${RED} $_uid ${ORG}"
-echo -e "Group:${RED} $_group${ORG}\n"
+echo -e "User name: ${RED}$user${ORG}"
+echo -e "User name ID (UID): ${RED}$uid${ORG}"
+echo -e "Group: ${RED}$group${ORG}"
 
-## VPN enum
+# Check VPN connection
 echo -e "${GREEN}====== VPN Enum ======${ORG}"
-
-if (ip a | grep -iq tun); then
-	echo -e "You appear to be connected to the VPN.${YELLOW}"; ip a | grep tun | awk '{print $2}' | sed 'N;s/\n/ /'; echo -e "${ORG}"
-    else
-	        echo -e "${RED}You do not appear to be connected to the VPN. ${ORG}"
-	 sleep 1s
- exit 1
+vpn_interfaces=$(ip a | grep -ci tun)
+if [ "$vpn_interfaces" -gt 1 ]; then
+    if [ "$vpn_interfaces" -gt 2 ]; then
+        echo -e "${YELLOW}Warning: Multiple VPN interfaces detected.${ORG}"
+    fi
+    echo -e "You appear to be connected to the VPN."
+    ip a | grep tun | awk '{print $2}' | sed 'N;s/\n/ /'
+else
+    echo -e "${RED}You do not appear to be connected to the VPN. ${ORG}"
 fi
 
-_vpnf="$(find / -name '*.ovpn' 2>/dev/null)"
-echo -e "${GREEN}====== VPN Files ======${ORG}\n${RED}$_vpnf ${ORG}\n"
+# Prompt for OpenVPN configuration file location
+echo -e "${GREEN}====== VPN Connection File and Server Name ======${ORG}"
+read -p "Enter the path to the OpenVPN configuration file: " vpn_file
+if [ -f "$vpn_file" ]; then
+    echo -e "${RED}$vpn_file${ORG}"
+    vpn_server=$(grep '^remote ' "$vpn_file" | awk '{print $2}')
+    echo -e "VPN server name: ${RED}$vpn_server${ORG}"
+else
+    echo -e "${RED}Unable to determine the file used to connect to the VPN.${ORG}"
+fi
+# Display IP routes
+echo -e "${GREEN}====== IP Routes Enum ======${ORG}"
+ip route
 
-## Routes enum
-echo -e "${GREEN}====== IP Routes Enum ======${ORG}"; ip route
-
-## Machine accessibility checks
-
+# Prompt for target IP address and check reachability
 echo -e "\n${GREEN}====== Target availability ======${ORG}"
-read -p "Enter the IP address of the target: " target; ping -c4 $target
-echo -e "\n"; traceroute $target
+read -p "Enter the IP address of the target: " target
+if ping -c4 "$target" &> /dev/null; then
+    echo -e "${GREEN}The target is reachable.${ORG}"
+else
+    echo -e "${RED}The target is not reachable.${ORG}"
+fi
+traceroute "$target"
+
+# Perform fast scan on top 1000 ports
+echo -e "\n${GREEN}====== Fast scan on top 1000 ports ======${ORG}"
+nmap -T4 -A $target
